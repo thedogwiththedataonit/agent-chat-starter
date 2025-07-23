927cf26ae20a0079af062377da300981f2b4b9ab
Thomas Park <thomas0806.park@gmail.com>
2025-07-23 01:26:19 -0400

    supported models

--- app/api/models/route.ts
+++ app/api/models/route.ts
@@ -1,6 +1,6 @@
 import { gateway } from "@/lib/gateway";
 import { NextResponse } from "next/server";
-import { SUPPORTED_MODELS } from "@/lib/constants";
+//import { SUPPORTED_MODELS } from "@/lib/constants";
 
 export async function GET() {
   const allModels = await gateway.getAvailableModels();
