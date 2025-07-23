33515125687c50ea8c756f31d78b382137f0748e
Thomas Park <thomas0806.park@gmail.com>
2025-07-23 00:41:50 -0400

    remove sonnet 4

--- lib/constants.ts
+++ lib/constants.ts
@@ -10,7 +10,7 @@ export const SUPPORTED_MODELS = [
   "openai/gpt-o3",
   "anthropic/claude-3.5-sonnet",
   "anthropic/claude-3.7-sonnet",
-  "anthropic/claude-4-sonnet",
-  "xai/grok-4",
+  //"anthropic/claude-4-sonnet",
+  //"xai/grok-4",
   "moonshotai/kimi-k2"
 ];
